5e366ffb6fba217c278be1f5f91b53a00361b0be
eagles70 <jkkim70@gmail.com>
2021-11-17 11:52:15 +0900

    process, arrow

--- src/component/process/process.jsx
+++ src/component/process/process.jsx
@@ -8,40 +8,64 @@ const Process = () => {
       <div className={styles.title}>진행절차</div>
       <div className={styles.container}>
         <div className={styles.article}>
-          <span className={styles.card}>
-            <span className={styles.icon1}><i className="fas fa-box"></i></span>
-            <p>{data.process1.title1}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon1}><i className="fas fa-box"></i></span>
-            <p>{data.process1.title2}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon1}><i className="fas fa-box"></i></span>
-            <p>{data.process1.title3}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon1}><i className="fas fa-box"></i></span>
-            <p>{data.process1.title4}</p>
-          </span>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon1}><i className={data.process1.icon1}></i></span>
+              <p>{data.process1.title1}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon1}><i className={data.process1.icon2}></i></span>
+              <p>{data.process1.title2}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon1}><i className={data.process1.icon3}></i></span>
+              <p>{data.process1.title3}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon1}><i className={data.process1.icon4}></i></span>
+              <p>{data.process1.title4}</p>
+            </span>
+            <span className={styles.arrow1}><i className="fas fa-angle-double-right"></i></span>
+          </div>
         </div>
         <div className={styles.article}>
-          <span className={styles.card}>
-            <span className={styles.icon2}><i className="fas fa-box"></i></span>
-            <p>{data.process2.title1}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon2}><i className="fas fa-box"></i></span>
-            <p>{data.process2.title2}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon2}><i className="fas fa-box"></i></span>
-            <p>{data.process2.title3}</p>
-          </span>
-          <span className={styles.card}>
-            <span className={styles.icon2}><i className="fas fa-box"></i></span>
-            <p>{data.process2.title4}</p>
-          </span>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon2}><i className={data.process2.icon1}></i></span>
+              <p>{data.process2.title1}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon2}><i className={data.process2.icon2}></i></span>
+              <p>{data.process2.title2}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon2}><i className={data.process2.icon3}></i></span>
+              <p>{data.process2.title3}</p>
+            </span>
+            <span className={styles.arrow}><i className="fas fa-angle-double-right"></i></span>
+          </div>
+          <div className={styles.arrow_box}>
+            <span className={styles.card}>
+              <span className={styles.icon2}><i className={data.process2.icon4}></i></span>
+              <p>{data.process2.title4}</p>
+            </span>
+            <span className={styles.arrow1}><i className="fas fa-angle-double-right"></i></span>
+          </div>
         </div>
       </div>
     </div>
