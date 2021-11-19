f313dd6e45ac119d40885207a1a78eb399cbf466
eagles70 <jkkim70@gmail.com>
2021-11-19 16:14:44 +0900

    offcanvas 작업중

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import { Offcanvas } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = ({burger, moveTop, toggleBurger}) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const offCanvas = () => {
+    return (
+      <>
+        <Offcanvas show={show} placement="top" onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </Offcanvas.Body>
+        </Offcanvas>
+      </>
+    );
+  }
+
   return(
     <div className={styles.navbar}>
+      {offCanvas()}
       <div className={styles.logo} onClick={moveTop}>
         <span className={styles.icon}><i className="fas fa-parachute-box"></i></span>
         <span className={styles.title}>물플</span>
@@ -17,6 +41,7 @@ const Navbar = ({burger, moveTop, toggleBurger}) => {
       </div>
       <button className={styles.inquiry}>견적문의</button>
       <button className={styles.calling}>전화문의</button>
+      <button className={styles.offcanvas} onClick={handleShow}><i className="fas fa-bars"></i></button>
       <span onClick={toggleBurger}>
         <span className={!burger ? styles.burger_active : styles.burger}><i className="fas fa-bars"></i></span>
         <span className={burger ? styles.times_active : styles.times}><i className="fas fa-times"></i></span>
